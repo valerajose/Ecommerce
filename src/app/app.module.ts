c9e47a907edae68af93a0794bd33d6c8537f02b2
valerajose <valera09j@gmail.com>
2021-04-21 10:09:52 -0400

    page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { BackendModule } from './backend/backend.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    PagesModule,
+    BackendModule,
+    AppRoutingModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/backend/set-productos/set-productos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-productos',
+  templateUrl: './set-productos.component.html',
+  styleUrls: ['./set-productos.component.scss'],
+})
+export class SetProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
